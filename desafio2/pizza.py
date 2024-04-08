4f32234b6c567cec985121d56254f79c2c048f9b
contrerascastro <161642444+contrerascastro@users.noreply.github.com>
2024-04-07 22:35:49 -0400

    Add files via upload

---
+++ desafio2/pizza.py
@@ -0,0 +1,34 @@
+import sys
+
+class Pizza():
+    ingredientes = 3
+    precio = 10000
+    tamaño = "familiar"
+
+delgada = Pizza()
+tradicional = Pizza()
+
+ingredientes_vegetales = ["tomate", "aceitunas", "champiñones"]
+ingredientes_proteinas = ["pollo" , "vacuno", "carne vegetal"]
+
+print(Pizza.precio)
+print(Pizza.tamaño)
+
+
+""""
+print("Bienvenido a nuestra Pizzería")
+
+pedido = input("elija el tipo de pizza que quiere ordenar, tradicional o delgada")
+
+if pedido == "delgada": 
+    pedido = delgada
+    print("Elegiste una pizza delgada")
+elif pedido == "tradicional":
+    pedido = tradicional
+    print("Elegiste una pizza tradicional")
+else: ()
+
+input("Ahora elige un ingrediente proteico (pollo, vacuno o carne vegetal)")
+
+print(pedido)
+"""
