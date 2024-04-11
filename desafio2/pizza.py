7c20afabc33f1a1d721ef6fa9b08c710da52ba41
contrerascastro <161642444+contrerascastro@users.noreply.github.com>
2024-04-11 19:40:52 -0400

    Update pizza.py

--- desafio2/pizza.py
+++ desafio2/pizza.py
@@ -8,8 +8,8 @@ class Pizza():
 delgada = Pizza()
 tradicional = Pizza()
 
-ingredientes_vegetales = ["tomate", "aceitunas", "champiñones"]
-ingredientes_proteinas = ["pollo" , "vacuno", "carne vegetal"]
+#ingredientes_vegetales = ["tomate", "aceitunas", "champiñones"]
+#ingredientes_proteinas = ["pollo" , "vacuno", "carne vegetal"]
 
 print(Pizza.precio)
 print(Pizza.tamaño)
